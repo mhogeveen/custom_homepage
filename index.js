fb5217262d1862b1898d68457b7a1b04e46d0264
Maarten Hogeveen <maarten@Maartens-MacBook-Air.local>
2020-07-02 12:18:13 +0200

    Switch to sass & hook up search bar & different bgs

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
    const form = document.querySelector('#form')
+   const input = document.querySelector('#input')
 
    form.addEventListener('submit', (e) => {
       e.preventDefault()
-      window.open('https://www.google.com', '_blank')
+      window.open(`https://www.google.com/search?q=${input.value}`, '_self')
    })
+
+   const datetime = document.querySelector('#datetime')
+   const time = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
+   const date = new Date().toLocaleDateString([])
+   datetime.innerHTML = `<div><h2>${time}</h2><h3>${date}</h3></div>`
 })
