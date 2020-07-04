3aa8863485a42b79e88f4da7aa1318bdf62ac94c
Maarten Hogeveen <maarten@Maartens-MacBook-Air.local>
2020-07-04 11:02:45 +0200

    Add weather fetched from API

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
    const form = document.querySelector('#form')
    const input = document.querySelector('#input')
+   const weatherIcon = document.querySelector('#weather_icon')
+   const weatherTemp = document.querySelector('#weather_temp')
 
    form.addEventListener('submit', (e) => {
       e.preventDefault()
@@ -11,4 +13,21 @@ document.addEventListener('DOMContentLoaded', () => {
    const time = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
    const date = new Date().toLocaleDateString([])
    datetime.innerHTML = `<div><h2>${time}</h2><h3>${date}</h3></div>`
+
+   const setWeather = () => {
+      const city = 'hilversum'
+      fetch(
+         `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=d98b5e820cf42990cd49cddf707d4a3a`
+      )
+         .then((res) => res.json())
+         .then((res) => {
+            weatherIcon.setAttribute(
+               'src',
+               `http://openweathermap.org/img/wn/${res.weather[0].icon}@2x.png`
+            )
+            weatherTemp.innerHTML = `<h2>${Math.floor(res.main.temp)}</h2><h3>&deg;C</h3>`
+         })
+   }
+
+   setWeather()
 })
